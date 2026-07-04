35fd210423e65b5e16d8fb94fd3027485326cb0a
itsypalaak53 <palakkawarchouhan@gmail.com>
2026-07-04 18:52:00 +0530

    Add project files and folders

---
+++ js/index.js
@@ -0,0 +1,32 @@
+console.log("hello palak");
+
+/* multi line comment */
+
+// let , var , const
+
+
+// var - globle scop - redeclare ✅ , update ✅
+// let - block scop -  redeclare ❎ , update ✅
+// const - block scop - redeclare ❎ , update ❎
+
+
+// ------------ var ------------
+// var a = 10;
+// console.log(a);
+
+// var a = 20;
+// console.log(a);
+// a = 30;
+// console.log(a);
+
+
+// ------------ let ------------
+// let a = 33;
+// console.log(a);
+// a = 44;
+// console.log(a);
+
+
+// ------------ const ------------
+// const a = 55;
+// console.log(a);
